deeed30ff1f59fd9bc1e76d91b13b5d647abf8fe
Deborah Espíndola <deo2@cin.ufpe.br>
2020-11-23 13:09:32 -0300

    feature(project-base): add base folders based on edgar's keystone template

---
+++ server/routes/index.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const keystone = require('keystone');
+const cors = require('cors');
+
+const Post = keystone.list('Posts');
+
+module.exports = (app) => {
+  app.use(cors());
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+
+  app.get('/api/posts', (req, res) => {
+    Post.model.find((err, data) => {
+      if (err) {
+        res.status(500).send('DB Error');
+      } else {
+        res.send(data);
+      }
+    });
+  });
+
+  app.get('*', (req, res) => {
+		res.redirect('/');
+	});
+};
