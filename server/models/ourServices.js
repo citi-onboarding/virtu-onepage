21b44c97a55d0b0ce189ba1eb67f2bfae2bdfbb1
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2020-11-25 23:10:41 -0300

    create ourServices model

---
+++ server/models/ourServices.js
@@ -0,0 +1,19 @@
+const keystone = require('keystone');
+const { Types } = keystone.Field;
+
+const ourServices = new keystone.List('Services');
+
+
+ourServices.add({
+	image: {
+   		type: Types.CloudinaryImages, require: true, index: true, initial: true,
+ 		},
+	title: {
+		type: Types.Text, required: true, index: true, initial: true,
+		},
+	description: {
+		type: Types.Textarea, require: true, index: true, initial: true,
+		},
+});
+
+ourServices.register();
