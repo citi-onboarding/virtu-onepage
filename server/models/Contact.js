f5b6e842b139002a3f560bf9867ea5d15ab58d23
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2020-11-25 23:25:46 -0300

    create contact model

---
+++ server/models/Contact.js
@@ -0,0 +1,21 @@
+const keystone = require('keystone');
+cont { Types } = keystone.Field;
+
+const Contact = new keystone.List('Contact');
+
+Contact.add ({
+	instagramLink: {
+		type: Types.Url, required: true, index: true, initial: true, 
+	},
+    	linkedinLink: {
+		type: Types.Url, required: true, index: true, initial: true, 
+	},
+    	email: {
+		type: Types.Text, required: true, index: true, initial: true,
+	},
+    	adress: {
+		type: Types.Text, initial: true, required: true, index: true,
+		}
+});
+
+Contact.register();
