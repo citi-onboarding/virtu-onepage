974a2129ffff5fddc8dbea6de20b9e6826dbeb38
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2020-11-30 00:13:21 -0300

    feature(feat/testimonial): everything done

---
+++ server/models/Testimonial.js
@@ -0,0 +1,27 @@
+const keystone = require('keystone');
+const { Types } = keystone.Field;
+
+const Testimonial = new keystone.List('Testimonial', {
+	map: { name: 'title' }
+  });
+
+
+Testimonial.add({
+	image: {
+   		type: Types.CloudinaryImages, require: true, index: true, initial: true,
+ 		},
+	title: {
+		type: Types.Text, required: true, index: true, initial: true,
+        },
+    subtitle: {
+        type: Types.Text, required: true, index: true, initial: true,
+        },
+    reason: {
+        type: Types.Text, require: true, index: true, initial: true,
+        },
+	description: {
+		type: Types.Textarea, require: true, index: true, initial: true,
+		},
+});
+
+Testimonial.register();
