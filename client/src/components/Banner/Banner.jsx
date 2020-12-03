484404bdaa8c97473fabea3a167e5d89769337d7
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2020-12-03 18:34:02 -0300

    banner done, missing opacity and responsivity

--- client/src/components/Banner/Banner.jsx
+++ client/src/components/Banner/Banner.jsx
@@ -5,15 +5,19 @@ import React from 'react';
 import './Banner.css';
 
 import logo_white from '../../assets/virtu-logo 2.svg'
-import background from '../../assets/background.png'
-
 
 function Banner() {
   return (
       <div className="banner-section">
-        oi
+        <div className="text">
+          <h1>Aplicamos ciência e construímos resultados</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscin elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <button><h3 className = "button-text">Nossos serviços</h3></button>
+        </div>
+        <div className="rectangle">
+          <img id = "logo-white" src={logo_white} alt="logo branca"/>
+        </div>
       </div>
   )
 }
-
 export default Banner;
