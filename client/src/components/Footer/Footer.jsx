34b1bb2995985f2dd3203335471be77bc2c38ddd
Arthur Brito <arthurmedeiros32@gmail.com>
2020-12-03 12:37:56 -0300

    fix(footer-section): change hearth and code in text to icons instead of text

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -5,12 +5,20 @@ import React from 'react';
 import './Footer.css';
 
 import logo_white from '../../assets/virtu-logo 2.svg'
+import hearth from '../../assets/hearth.svg'
+import code_logo from '../../assets/code-icon.svg'
+
 
 function Footer() {
   return (
       <div className="footer-section">
         <img src={logo_white} alt="logo branca"/>
-        <p>Made with &lt;/&gt; and &#10084; by Citi</p>
+        <p>Made with 
+          <img src={hearth} alt="coracao" className="hearth-icon" />
+           and 
+          <img src={code_logo} alt="code" className="code_icon" />
+           by Citi
+        </p>
       </div>
   )
 }
