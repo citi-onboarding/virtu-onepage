adec04522576bef15fe998edd58c1399dc438cb0
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2020-12-04 00:32:52 -0300

    feature(blog-section): doing html

--- client/src/components/Blog/Blog.jsx
+++ client/src/components/Blog/Blog.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
+import circle from '../../assets/Grupo 6.png'
+import lines from '../../assets/Grupo 5.png'
+
 import './Blog.css';
 
 function Blog() {
   return (
       <div className="blog-section">
-        {/* hi */}
+        <div className="head">
+          <img src={lines} alt="linhas abstratas"/>
+          <div className="title">
+            <h1>Conheça nosso blog</h1>
+            <a href="">Visitar blog</a>
+          </div>
+          <img src={circle} alt=""/>
+        </div>
       </div>
   )
 }
