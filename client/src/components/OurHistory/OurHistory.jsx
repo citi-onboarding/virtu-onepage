66ec17bee8403a58b55bf0e595cf3cff99039321
Shellyda Barbosa <shellydabarbosa@gmail.com>
2020-12-04 00:24:54 -0200

    update(ourhistory): putting the icons in their positions

--- client/src/components/OurHistory/OurHistory.jsx
+++ client/src/components/OurHistory/OurHistory.jsx
@@ -4,7 +4,7 @@ import './css/ourhistory.css'
 
 import vector_left from '../../assets/Grupo 5SVG.svg'//triangulo
 import vector_right from '../../assets/Caminho 5SVG.svg' //caminho
-import vector_midle from   '../../assets/Caminho 4SVG.svg'//triangulos meio
+import vector_mid from   '../../assets/Group 4SVG.svg'//triangulos meio
 
 import icon_eye from '../../assets/eye 2SVG.svg'
 import icon_hands from '../../assets/handshake 2SVG.svg'
@@ -15,7 +15,7 @@ const titles = [
 ]
 
 const icons = [
-    icon_eye, icon_hands, icon_eye
+    icon_eye, icon_hands, icon_arrow
 ]
 
 const text = [
@@ -27,16 +27,24 @@ const bgColor = [
 'linearGradient(#cc1701 90%, #991200 90%)'
 ]
 
+const position = [
+'relative'
+]
+
 const OurHistory = () => (
 <section id="ourhistory">
 <div className="container">
 <div className="call">
+<img src={vector_left} alt="vector" />
 <h1>Conheça nossa história</h1>
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 </div>
 <div className="box-parent">
-<Box title={titles[0]} icons={icons[0]} text={text[0]} bgColor={bgColor[0]} />
+<Box title={titles[0]} icons={icons[0]} text={text[0]} bgColor={bgColor[0]} position={position[0]} />
+<Box title={titles[0]} icons={icons[0]} text={text[0]} bgColor={bgColor[0]}/>
+<Box title={titles[0]} icons={icons[0]} text={text[0]} bgColor={bgColor[0]} position={position[0]} />
 </div>
+<div className="img-mid"><img src={vector_mid} alt="vector" /></div>
 </div>
 </section>
 )
