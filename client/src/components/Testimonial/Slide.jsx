60efb50e3eb2714bca64530f752b0eabb03cf578
Shellyda Barbosa <shellydabarbosa@gmail.com>
2020-12-05 16:45:37 -0200

    update(testimonial): getting the idea

--- client/src/components/Testimonial/Slide.jsx
+++ client/src/components/Testimonial/Slide.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import './css/slide.css'
+import img_1 from './images/image 3.svg'
+import img_2 from './images/image 4.svg' 
+
 
 export default class Slide extends React.Component{
 render(){
 return(
-<div className="box-single" >
-
+<div className="nav-galeria" >
+	<div className="nav-galeria-wraper">
+		<div className="mini-img-wraper">
+				<div className="mini-client-img"></div>
+				<h2>{this.props.Name}</h2>
+				<p>{this.props.Func}</p>
+		</div>
+	</div>
 </div>
 )
 }
--- client/src/components/Testimonial/Testimonial.jsx
+++ client/src/components/Testimonial/Testimonial.jsx
@@ -6,19 +6,37 @@ import icon_right from '../../assets/Group 4SVG.svg'
 import arrow_left from '../../assets/arrow leftSVG.svg'
 import arrow_right from '../../assets/arrow rightSVG.svg'
 
+
+
+const Name = [
+    'Isabela Souza'
+]
+
+const Func = [
+	'Candidata a vereadora'
+]
+
+const Service = [
+	'Pesquisa de opinião'
+] 
+
+const Desc = [
+	'Lorem ipsum dolor sit amet, consectetur adipi scing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'
+]
 const Testimonial = () => (
 <section id="testimonial">
 <div className="container">
 <div className="parent-box">
  	<div className="box-right">
- 	<h1>Depoimentos <br></br> dos nossos clientes</h1>
+ 		<h1>Depoimentos <br></br> dos nossos clientes</h1>
 
- 		<h2>Isabela Souza</h2>
- 		<p id="p-1">Candidata a vereadora</p>
- 		<p id="p-2">Contratou: Pesquisa de opinião</p>
+ 		<h2>{Name[0]}</h2>
+ 		<p id="p-1">{Func[0]}</p>
+ 		<p id="p-2">Contratou: {Service[0]}</p>
 
- 		<p id="p-3">Lorem ipsum dolor sit amet, consectetur adipi scing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+ 		<p id="p-3">{Desc[0]}</p>
  	</div>{/*fotodestaque*/}
+ 	<Slide Name={Name[0]} Func={Func[0]} />
 </div>
 </div>{/*container*/}
 </section>
