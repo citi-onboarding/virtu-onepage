7fff316cd5d96f120feba33f532a684684c33a20
Shellyda Barbosa <shellydabarbosa@gmail.com>
2020-12-05 14:49:46 -0200

    update(testimonial): slide base

--- client/src/components/Testimonial/Testimonial.jsx
+++ client/src/components/Testimonial/Testimonial.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
-
+import Slide from './Slide'
 import './css/Testimonial.css'
 
+import icon_right from '../../assets/Group 4SVG.svg'
+import arrow_left from '../../assets/arrow leftSVG.svg'
+import arrow_right from '../../assets/arrow rightSVG.svg'
 
 const Testimonial = () => (
 <section id="testimonial">
 <div className="container">
-    <h1> alo</h1>
+    <h1>Depoimentos <br></br> dos nossos clientes</h1>
+<div className="parent-box">
+ 	<div className="box-right">
+ 		<h2>Isabela Souza</h2>
+ 		<p id="p-1">função</p>
+ 		<p id="p-2">contrato</p>
+
+ 		<p id="p-3"></p>
+ 	</div>{/*fotodestaque*/}
+</div>
 </div>{/*container*/}
 </section>
 )
