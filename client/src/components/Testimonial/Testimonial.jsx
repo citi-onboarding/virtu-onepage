197c7e39b2605f2a100af0c59f29bf272e0bf2d2
Shellyda Barbosa <shellydabarbosa@gmail.com>
2020-12-05 14:08:58 -0200

    feature(testimonial): base files

---
+++ client/src/components/Testimonial/Testimonial.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './css/testimonial.css'
+
+
+const Testimonial = () => (
+<section id="testimonial">
+<div className="container">
+
+</div>{/*container*/}
+</section>
+)
+
+export default Testimonial
