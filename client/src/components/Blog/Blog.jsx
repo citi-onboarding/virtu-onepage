c30f4d591c140f8cba2a2d6a6d6fcd9fd39884fb
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2020-12-06 18:54:00 -0300

    feat(blog-section): creating component to carousel

--- client/src/components/Blog/Blog.jsx
+++ client/src/components/Blog/Blog.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import circle from '../../assets/Grupo 6.png'
 import lines from '../../assets/Grupo 5.png'
+import Datas from './Datas'
 
 import './Blog.css';
 
@@ -11,6 +12,22 @@ const title = [
 
 const subtitle = [
   'Visitar blog'
+]
+
+const titleBlog = [
+  'Título do blog título do blog título do blog',
+  'Título do blog título do blog título do blog',
+  'Título do blog título do blog título do blog',
+]
+
+const textBlog = [
+  'Lorem ipsum dolor sit amet, consectetur adipi scing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+  'Lorem ipsum dolor sit amet, consectetur adipi scing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+  'Lorem ipsum dolor sit amet, consectetur adipi scing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+]
+
+const clickHere = [
+  'Ler post completo'
 ]
 
 function Blog() {
@@ -23,6 +40,23 @@ function Blog() {
             <a href="">{subtitle}</a>
           </div>
           <img id="circle" src={circle} alt=""/>
+        </div>
+        <div className="carousel">
+          <div>
+            <h2>{titleBlog[0]}</h2>
+            <p>{textBlog[0]}</p>
+            <a href="">{clickHere}</a>
+          </div>
+          <div>
+            <h2>{titleBlog[1]}</h2>
+            <p>{textBlog[1]}</p>
+            <a href="">{clickHere}</a>
+          </div>
+          <div>
+            <h2>{titleBlog[2]}</h2>
+            <p>{textBlog[2]}</p>
+            <a href="">{clickHere}</a>
+          </div>
         </div>
       </div>
   )
---
+++ client/src/components/Blog/Datas/Datas.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './Datas.css';
+
+function Datas() {
+    return (
+        <div className="data">
+            <div>
+            <h2>props.text</h2>
+            <p>props.text</p>
+            <a href="">props.text</a>
+          </div>
+        </div>
+    )
+}
+
+export default Datas;
