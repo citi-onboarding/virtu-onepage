a06127bf6efeef62b1b95318981bb0dd0d289ce5
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2020-12-07 00:10:11 -0300

    feat(blog-section): trying to create slick

--- client/src/components/Blog/Blog.jsx
+++ client/src/components/Blog/Blog.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import circle from '../../assets/Grupo 6.png'
 import lines from '../../assets/Grupo 5.png'
+
+import "~slick-carousel/slick/slick.css"; 
+import "~slick-carousel/slick/slick-theme.css";
+
+import Slider from 'react-slick';
 
 import Datas from './Datas'
 
@@ -16,15 +21,31 @@ const subtitle = [
 ]
 
 const titleBlog = [
-  'Título do blog título do blog título do blog'
+  'Título do blog título do blog título do blog',
 ]
 
 const text = [
   'Lorem ipsum dolor sit amet, consectetur adipi scing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'
 ]
 
-function Blog() {
-  return (
+const link = [
+  'https://react-slick.neostack.com/docs/get-started/'
+]
+
+class Blog extends Component {
+  
+  render() {
+
+    const settings = {
+      className: "center",
+      centerMode: true,
+      infinite: true,
+      centerPadding: "60px",
+      slidesToShow: 3,
+      speed: 500
+    }
+    
+    return (
       <div className="blog-section">
         <div className="head">
           <img id="lines" src={lines} alt="linhas abstratas"/>
@@ -35,12 +56,16 @@ function Blog() {
           <img id="circle" src={circle} alt=""/>
         </div>
         <div className="carousel">
-          <Datas title={titleBlog} text={text} />
-          <Datas title={titleBlog} text={text} />
-          <Datas title={titleBlog} text={text} />
+          <Slider {...settings}>
+            <Datas title={titleBlog} text={text} link={link}/>
+            <Datas title={titleBlog} text={text} link={link}/> 
+            <Datas title={titleBlog} text={text} link={link}/>
+          </Slider>
+          
         </div>
       </div>
-  )
+    )
+  }
 }
 
 export default Blog;
