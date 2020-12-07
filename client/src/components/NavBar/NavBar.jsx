3534d6c9f8eeab650e2a4c5500f7f0d45392622a
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2020-12-07 12:49:57 -0300

    feature(nav-bar): doing css

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -7,17 +7,19 @@ import './NavBar.css';
 function NavBar() {
   return (
       <header className="navbar-section">
-        <img id="logo" src={virtu_logo} alt="Logo da Virtù"/>
-        <div className="sections">
-          <ul>
-            <li><a href="">Quem Somos</a></li>
-            <li><a href="">Serviços</a></li>
-            <li><a href="">Posts</a></li>
-            <li><a href="">Depoimentos</a></li>
-            <li><a href="">Fale Conosco</a></li>
-          </ul>
-          
+        <div className="connect">
+          <img id="logo" src={virtu_logo} alt="Logo da Virtù"/>
+          <div className="sections">
+            <ul>
+              <li><a href="">Quem Somos</a></li>
+              <li><a href="">Serviços</a></li>
+              <li><a href="">Posts</a></li>
+              <li><a href="">Depoimentos</a></li>
+              <li><a href="">Fale Conosco</a></li>
+            </ul>
         </div>
+      </div>
+        
       </header>
   )
 }
