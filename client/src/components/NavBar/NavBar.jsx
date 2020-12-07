4fe6e3e489cba7ed5b3b4d5f46959b7c8cd225b0
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2020-12-07 15:54:47 -0300

    update(nav-bar/banner): apply prettier

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,27 +1,38 @@
-import React from 'react';
+import React from "react";
 
-import virtu_logo from '../../assets/virtu-logo 3.svg'
+import virtu_logo from "../../assets/virtu-logo 3.svg";
 
-import './NavBar.css';
+import "./NavBar.css";
 
 function NavBar() {
   return (
-      <header className="navbar-section">
-        <div className="connect">
-          <img id="logo" src={virtu_logo} alt="Logo da Virtù"/>
-          <div className="sections">
-            <ul>
-              <li><a href="">Quem Somos</a></li>
-              <li><a href="">Serviços</a></li>
-              <li><a href="">Posts</a></li>
-              <li><a href="">Depoimentos</a></li>
-              <li><a href="">Fale Conosco</a></li>
-            </ul>
+    <header className="navbar-section">
+      <div className="connect">
+        <img id="logo" src={virtu_logo} alt="Logo da Virtù" />
+        <div className="sections">
+          <ul>
+            <li>
+              <a href="">Quem Somos</a>
+            </li>
+            <li>
+              <a href="">Serviços</a>
+            </li>
+            <li>
+              <a href="">Posts</a>
+            </li>
+            <li>
+              <a href="">Depoimentos</a>
+            </li>
+            <li>
+              <a id="last" href="">
+                Fale Conosco
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
-        
-      </header>
-  )
+    </header>
+  );
 }
 
 export default NavBar;
