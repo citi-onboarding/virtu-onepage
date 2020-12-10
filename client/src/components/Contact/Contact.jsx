1f5e7a1ed5896c95643f95524591a8319240e6ef
Arthur Brito <arthurmedeiros32@gmail.com>
2020-12-10 14:00:39 -0300

    feature(Contact-section): insert images in form, therefore, finish desktop version

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -3,6 +3,8 @@ import icon_linkedin from '../../assets/Icone LinkedinSVG.svg'
 import icon_instagram from '../../assets/Icone InstaSVG.svg'
 import icon_email from '../../assets/Icone e-mailSVG.svg'
 import icon_adress from '../../assets/Icone enderecoSVG.svg'
+import icon_ball from '../../assets/Grupo 6SVG.svg'
+import icon_lines from '../../assets/Grupo 5SVG.svg'
 
 import './Contact.css'
 
@@ -67,8 +69,10 @@ function Contact() {
                 <img src={icon_adress} alt="endereço"/>
                 <a href="#">Av. Professor Luiz Freire, Cidade Universitaria, CFCH, Recife - PE</a>
             </div>
-            
         </div>
+
+        <img src={icon_ball} alt="ball" id="ball-form"/>
+        <img src={icon_lines} alt="lines" id="lines-form"/>
     </div>
   );
 }
