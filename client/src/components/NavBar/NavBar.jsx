4bb430d8698b13985c74cd4f8a5dbe083e77d076
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2020-12-11 00:25:04 -0300

    feature(nav-bar-navigate): change screen WORKING! :)

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -3,25 +3,25 @@ import React, { useState, useEffect } from "react";
 import virtu_logo from "../../assets/virtu-logo 3.svg";
 import menu from "../../assets/Menu.svg";
 import ex from "../../assets/Ex.png";
+import insta from "../../assets/Icone InstaSVG.svg";
+import email from "../../assets/Icone e-mail.svg";
 
 import "./NavBar.css";
 
 function NavBar() {
 
   const [mode, setMode] = useState({menu});
-
+  
   return (
     <> 
     <header className="navbar-section">
       <div className="connect">
         <img id="logo" src={virtu_logo} alt="Logo da Virtù" />
-        <div className="sections">
-          <div className="icon" onClick = {() => setMode(!mode)} >
-  
-           { mode ?  <img src={menu} alt="Menu"/>  : <img src={ex} alt="Sair" /> }
-            
+        <div className="icon" onClick = {() => setMode(!mode)} >
+        { mode ?  <img src={menu} alt="Menu"/>  : <img src={ex} alt="Sair" /> }
           </div>
-         
+        <div className="sections" style={{display: (mode ? 'none' : 'flex')}} >
+      
           <ul className="list">
             <li>
               <a className = "current" href="">Quem Somos</a>
@@ -38,6 +38,13 @@ function NavBar() {
             <li>
               <a href=""> Fale Conosco</a>
             </li>
+            <div className="on-mobile">
+              <ul className="more-contact">
+                <li><p>@virtupolitica</p><img src={insta} alt="Instagram"/></li>
+                <li><p>virtu@email.com</p><img src={email} alt="Email"/> </li>
+                
+              </ul>
+            </div>
           </ul>
         </div>
       </div>
