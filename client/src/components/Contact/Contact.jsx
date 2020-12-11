db2aee5013756edf00a144f0155ef73ecb57e821
Arthur Brito <arthurmedeiros32@gmail.com>
2020-12-10 23:04:09 -0300

    feature(Contact-section): make input value change dinamicly

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -12,80 +12,90 @@ import Input from '../Input';
 
 function Contact() {
     const [service, setService] = useState('');
-    const [name, setName] = useState('');
+    const [name, setName] = useState("");
     const [telephone, setTelephone] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
 
-  return (
-    <div id="contact-section">
-        <form>
-            <fieldset>
-                <legend>Fale conosco</legend>
+    return (
+        <div id="contact-section">
+            <form>
+                <fieldset>
+                    <legend>Fale conosco</legend>
 
-                <div className="input-items">
-                    <div className="name-telephone-container">
-                        <Input name="name" placeholder="Nome" width="180px"
-                            onChange={(e) => {setName(e.target.value)}}
+                    <div className="input-items">
+                        <div className="name-telephone-container">
+                            <Input 
+                                name="name" 
+                                placeholder="Nome" 
+                                width="180px" 
+                                value={name}
+                                funcOnChange={(e) => { setName(e.target.value);}}  
+                            />
+
+                            <Input 
+                                name="telephone" 
+                                placeholder="Telefone" 
+                                width="180px"
+                                value={telephone}
+                                funcOnChange={(e) => {setTelephone(e.target.value)}}
+                            />
+                        </div>
+
+                        <Input name="email" placeholder="Email" width="100%" marginTop="30px" value={email} 
+                            funcOnChange={(e) => {setEmail(e.target.value)}}
                         />
 
-                        <Input name="telephone" placeholder="Telefone" width="180px"
-                            onChange={(e) => {setTelephone(e.target.value)}}
-                        />
+                        <div className="input-line-container">        
+                            <select 
+                                name="services"
+                                onChange={(e) => {setService(e.target.value);} }
+                            >
+                                <option value="" disabled hidden>Serviço</option>
+                                <option value="test">test</option>
+                                <option value="test2">test2</option>
+                            </select>
+
+                            <div className="white-line"></div>
+                        </div>
+
+                        <div className="input-line-container">
+                            <textarea name="message" id="message" cols="30" rows="10" placeholder="Mensagem"
+                                onChange={(e) => { setMessage(e.target.value) }}
+                            />    
+                            <div className="white-line"></div>
+                        </div>
                     </div>
 
-                    <Input name="email" placeholder="Email" width="100%" marginTop="30px" 
-                        onChange={(e) => {setEmail(e.target.value)}}
-                    />
+                    <button type="submit">Enviar</button>
+                </fieldset>
+            </form>
 
-                    <div className="input-line-container">        
-                        <select value={service} name="services"
-                         onChange={(e) => { setService(e.target.value) }}>
-                            <option value="" disabled hidden>Serviço</option>
-                            <option value="test">test</option>
-                            <option value="test2">test2</option>
-                        </select>
+            <div className="links-section">
+                <h2>Entre em contato</h2>
 
-                        <div className="white-line"></div>
-                    </div>
-
-                    <div className="input-line-container">
-                        <textarea name="message" id="message" cols="30" rows="10" placeholder="Mensagem"
-                            onChange={(e) => { setMessage(e.target.value) }}
-                        />    
-                        <div className="white-line"></div>
-                    </div>
+                <div className="link-image-container">
+                    <img src={icon_linkedin} alt="linkedin"/>
+                    <a href="#">.../company/virtu-consultoriapolitica</a>
                 </div>
+                <div className="link-image-container">
+                    <img src={icon_instagram} alt="instagram"/>
+                    <a href="#">@virtupolitica</a>
+                </div>
+                <div className="link-image-container">
+                    <img src={icon_email} alt="email"/>
+                    <a href="#">virtu@email.com</a>
+                </div>
+                <div className="link-image-container">
+                    <img src={icon_adress} alt="endereço"/>
+                    <a href="#">Av. Professor Luiz Freire, Cidade Universitaria, CFCH, Recife - PE</a>
+                </div>
+            </div>
 
-                <button type="submit">Enviar</button>
-            </fieldset>
-        </form>
-
-        <div className="links-section">
-            <h2>Entre em contato</h2>
-
-            <div className="link-image-container">
-                <img src={icon_linkedin} alt="linkedin"/>
-                <a href="#">.../company/virtu-consultoriapolitica</a>
-            </div>
-            <div className="link-image-container">
-                <img src={icon_instagram} alt="instagram"/>
-                <a href="#">@virtupolitica</a>
-            </div>
-            <div className="link-image-container">
-                <img src={icon_email} alt="email"/>
-                <a href="#">virtu@email.com</a>
-            </div>
-            <div className="link-image-container">
-                <img src={icon_adress} alt="endereço"/>
-                <a href="#">Av. Professor Luiz Freire, Cidade Universitaria, CFCH, Recife - PE</a>
-            </div>
+            <img src={icon_ball} alt="ball" id="ball-form"/>
+            <img src={icon_lines} alt="lines" id="lines-form"/>
         </div>
-
-        <img src={icon_ball} alt="ball" id="ball-form"/>
-        <img src={icon_lines} alt="lines" id="lines-form"/>
-    </div>
-  );
+    );
 }
 
 export default Contact;
--- client/src/components/Input/Input.jsx
+++ client/src/components/Input/Input.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 import './Input.css';
 
-function Input({ name, placeholder, width, marginTop }) {
+function Input({ name, placeholder, width, marginTop, value, funcOnChange }) {
     return (
         <div className="input-line-container">
-            <input type="text" name={name} placeholder={placeholder} style={{width: width, marginTop: marginTop}} />
+            <input type="text" value={value} name={name} placeholder={placeholder} style={{width: width, marginTop: marginTop}} onChange={funcOnChange} />
             <div className="white-line"></div>
         </div>
     );
