21ee22ae7b54b3bd12eca9e11453100969a899b7
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2020-12-13 23:23:21 -0300

    feature(nav-bar-navigate): hamburguer working

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -10,46 +10,78 @@ import "./NavBar.css";
 
 function NavBar() {
   const [icon, setIcon] = useState(true);
-  const [mobile, setMobile] = useState(false);
 
   return (
     <>
       <header className="navbar-section">
         <div className="connect">
-                    <div className="on-web">
-                        <img id="logo" src={virtu_logo} alt="Logo da Virtù" />
-                        <ul>
-                        <li><a href="#ourhistory">Quem Somos</a></li>
-                        <li><a href="#services">Serviços</a></li>
-                        <li><a href="#linkedin">LinkedIn</a></li>
-                        <li><a href="#testimonials">Depoimentos</a></li>
-                        <li><a href="#contact-section"> Fale Conosco</a></li>
-                        </ul>
-                    </div>
-                        
-                                <div className="on-mobile">
-                                    <ul>
-                                        <li><a href="#ourhistory">Quem Somos</a></li>
-                                        <li><a href="#services">Serviços</a></li>
-                                        <li><a href="#linkedin">LinkedIn</a></li>
-                                        <li><a href="#testimonials">Depoimentos</a></li>
-                                        <li><a href="#contact-section"> Fale Conosco</a></li>
-                                    </ul>
-                                        <img id="logo-mobile" src={virtu_logo} alt="" />
-                                            <div className="icon" onClick={() => setIcon(!icon)}>
-                                                {icon ? <img src={menu} alt="Menu" /> : <img src={ex} alt="Sair" />}
-                                            </div>
-                                        <ul className="more-contact">
-                                        <li>
-                                            <p>@virtupolitica</p>
-                                            <img src={insta} alt="Instagram" />
-                                        </li>
-                                        <li>
-                                            <p>virtu@email.com</p>
-                                            <img src={email} alt="Email" />
-                                        </li>
-                                        </ul>
-                                </div>
+          <div className="on-web">
+            <img id="logo" src={virtu_logo} alt="Logo da Virtù" />
+            <ul>
+              <li>
+                <a href="#ourhistory">Quem Somos</a>
+              </li>
+              <li>
+                <a href="#services">Serviços</a>
+              </li>
+              <li>
+                <a href="#linkedin">LinkedIn</a>
+              </li>
+              <li>
+                <a href="#testimonials">Depoimentos</a>
+              </li>
+              <li>
+                <a href="#contact-section"> Fale Conosco</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="on-mobile">
+            <div className="icon" onClick={() => setIcon(!icon)}>
+              {icon ? (
+                <img src={menu} alt="Menu" />
+              ) : (
+                <img src={ex} alt="Sair" />
+              )}
+            </div>
+
+            <div className={ icon ? 'closed-menu' : 'open-menu'}>
+
+                <ul>
+                <li>
+                    <a href="#ourhistory">Quem Somos</a>
+                </li>
+                <li>
+                    <a href="#services">Serviços</a>
+                </li>
+                <li>
+                    <a href="#linkedin">LinkedIn</a>
+                </li>
+                <li>
+                    <a href="#testimonials">Depoimentos</a>
+                </li>
+                <li>
+                    <a href="#contact-section"> Fale Conosco</a>
+                </li>
+                </ul>
+
+                <footer>
+                <img id="logo-mobile" src={virtu_logo} alt="" />
+                <ul className="more-contact">
+                    <li>
+                    <p>@virtupolitica</p>
+                    <img src={insta} alt="Instagram" />
+                    </li>
+                    <li>
+                    <p>virtu@email.com</p>
+                    <img src={email} alt="Email" />
+                    </li>
+                </ul>
+                </footer>
+
+            </div>
+
+          </div>
         </div>
       </header>
     </>
