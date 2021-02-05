fc0f636bf26a63042c18b2aa1f0f139b971d7354
Arthur Brito <arthurmedeiros32@gmail.com>
2021-02-05 17:26:34 -0300

    update(side-lines): update names of variables and indentation

--- client/src/components/OurHistory/OurHistory.jsx
+++ client/src/components/OurHistory/OurHistory.jsx
@@ -5,51 +5,51 @@ import axios from 'axios'
 import url from '../../global/globalVars';
 
 
-import vector_left from '../../assets/Grupo 5SVG.svg'//triangulo
-import vector_right from '../../assets/Caminho 6 rotate-left.png' //caminho
-import vector_mid from   '../../assets/Group 4SVG.svg'//triangulos meio
+import waves_left from '../../assets/Grupo 5SVG.svg'//triangulo
+import line_right from '../../assets/Caminho 1SVG.svg' //caminho
+import triangles_mid from   '../../assets/Group 4SVG.svg'//triangulos meio
 
 import icon_eye from '../../assets/eye 2SVG.svg'
 import icon_hands from '../../assets/handshake 2SVG.svg'
 import icon_arrow from'../../assets/arrowSVG.svg'
 
 function OurHistory (){
-const[ourhistory, setOurHistory] = useState([])
+	const[ourhistory, setOurHistory] = useState([])
 
-    const LoadOurHistory = async () =>{
-      const res = await axios.get(`${url}/api/ourhistory`)
-      setOurHistory(res.data)
-    } 
+		const LoadOurHistory = async () =>{
+		const res = await axios.get(`${url}/api/ourhistory`)
+		setOurHistory(res.data)
+		} 
 
-    useEffect(()=>{
-      LoadOurHistory()
-    }, [])
+		useEffect(()=>{
+		LoadOurHistory()
+		}, [])
 
-return(
-<section id="ourhistory">
-	<div className="container-ourhistory">
-		<div className="img-left"><img src={vector_left} alt="vector"/></div>
-		{ourhistory?.map(Stories => { 
-			return(	 
-				<div className="call">
-					<h1>Conheça nossa história</h1>
-					<p>{Stories.NossaHistoria}</p>
-				</div>
-			)
-		})}
-		{ourhistory?.map(Stories => { 
-			return(
-				<div className="box-parent">
-					<Box title="Missão" icons={icon_arrow} text={Stories.Valores}  position="relative" />
-					<Box title="Visão" icons={icon_hands} text={Stories.Visao} bgColor="#D55140"/>
-					<Box title="Valores" icons={icon_eye} text={Stories.Missao} position="relative" />
-				</div>
-			)
-		})}
-	<div className="img-mid"><img src={vector_mid} alt="vector" /></div>
-</div>
-	<img className="img-right" src={vector_right} alt="vector"/>
-</section>
-)
+	return(
+		<section id="ourhistory">
+			<div className="container-ourhistory">
+				<div className="img-left"><img src={waves_left} alt="vector"/></div>
+				{ourhistory?.map(Stories => { 
+					return(	 
+						<div className="call">
+							<h1>Conheça nossa história</h1>
+							<p>{Stories.NossaHistoria}</p>
+						</div>
+					)
+				})}
+				{ourhistory?.map(Stories => { 
+					return(
+						<div className="box-parent">
+							<Box title="Missão" icons={icon_arrow} text={Stories.Valores}  position="relative" />
+							<Box title="Visão" icons={icon_hands} text={Stories.Visao} bgColor="#D55140"/>
+							<Box title="Valores" icons={icon_eye} text={Stories.Missao} position="relative" />
+						</div>
+					)
+				})}
+			<div className="img-mid"><img src={triangles_mid} alt="vector" /></div>
+		</div>
+			<img className="img-right" src={line_right} alt="vector"/>
+		</section>
+	)
 }
 export default OurHistory;
